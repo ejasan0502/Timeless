2d0c97409852c3dd03404bddc6c67b0438eeaf4b
Cire <esantiago@lockeddoorstudios.com>
2016-11-30 14:04:15 -0800

    login

---
+++ Assets/Scripts/Network/Account.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Account {
+    
+    public readonly string email;
+    public readonly string username;
+    public readonly string password;
+
+    public Account(string email, string username, string password){
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
