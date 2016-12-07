19b6ed4a302f349ae0fe01d4c83c09da5077b7ad
Cire <esantiago@lockeddoorstudios.com>
2016-12-07 11:17:04 -0800

    player movement

---
+++ Assets/Scripts/Player/CameraControl.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    void Update(){
+        transform.LookAt(transform.parent);
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Collections;
+using MassiveNet;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerInput : MonoBehaviour {
+
+    public float speed = 500f;
+    private CharacterController cc;
+
+    private Vector3 moveTo = Vector3.zero;
+
+    void Awake(){
+        cc = GetComponent<CharacterController>();
+        moveTo = transform.position;
+    }
+    void Update(){
+        Movement();
+    }
+
+    private void Movement(){
+        if ( Input.GetMouseButtonDown(0) ){
+            RaycastHit hit;
+            if ( Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000f, 1 << LayerMask.NameToLayer("Terrain")) ){
+                moveTo = hit.point;
+            }
+        }
+
+        cc.SimpleMove( (moveTo-transform.position).normalized*speed*Time.deltaTime );
+    }
+}
