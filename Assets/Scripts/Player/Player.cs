e4f709652c68ca9654e7ac8cf0ad35fdae6b56f0
Cire <esantiago@lockeddoorstudios.com>
2017-01-13 16:41:41 -0800

    passive skills

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,4 +7,18 @@ public class Player : MonoBehaviour {
     public CharStats baseCharStats;
     public EquipStats baseEquipStats;
 
+    private SkillLibrary skillLib;
+    private Character character;
+
+    void Awake(){
+        skillLib = GetComponent<SkillLibrary>();
+        character = GetComponent<Character>();
+    }
+
+    public void UpdateStats(){
+        character.maxStats = baseCharStats;
+        character.maxEquipStats = baseEquipStats;
+
+        skillLib.ApplyPassives(character);
+    }
 }
--- Assets/Scripts/Player/SkillLibrary.cs
+++ Assets/Scripts/Player/SkillLibrary.cs
@@ -8,8 +8,20 @@ public class SkillLibrary : MonoBehaviour {
     
     public List<Skill> skills { get; private set; }
 
+    void Awake(){
+        skills = new List<Skill>();
+    }
+
+    public bool HasSkill(string id){
+        return skills.Where(s => s.id == id).FirstOrDefault<Skill>() != null;
+    }
     public void AddSkill(string id){
-        
+        if ( HasSkill(id) ) return;
+
+        Skill skill = SkillManager.GetSkill(id);
+        if ( skill != null ){
+            skills.Add(skill);
+        }
     }
     public int GetSkillIndex(Skill s){
         for (int i = 0; i < skills.Count; i++){
@@ -17,5 +29,31 @@ public class SkillLibrary : MonoBehaviour {
                 return i;
         }
         return -1;
+    }
+    public void ApplyPassives(Character caster){
+        List<PassiveSkill> passiveSkills = new List<PassiveSkill>();
+        foreach (Skill s in skills){
+            if ( s.skillType == SkillType.passive )
+                passiveSkills.Add((PassiveSkill)s.Self);
+        }
+
+        CharStats charStats = new CharStats(0f);
+        EquipStats equipStats = new EquipStats(0f);
+        CharStats charStatsP = new CharStats(1f);
+        EquipStats equipStatsP = new EquipStats(1f);
+        foreach (PassiveSkill s in passiveSkills){
+            if ( s.percent ){
+                charStatsP += s.charStats;
+                equipStatsP += s.equipStats;
+            } else {
+                charStats += s.charStats;
+                equipStats += s.equipStats;
+            }
+        }
+
+        caster.maxStats += charStats;
+        caster.maxEquipStats += equipStats;
+        caster.maxStats *= charStatsP;
+        caster.maxEquipStats *= equipStatsP;
     }
 }
