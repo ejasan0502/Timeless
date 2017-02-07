e519b02b1af2383683b4ef4c355a1829b0cecd58
Eric <esantiago@lockeddoorstudios.com>
2017-02-07 01:50:12 -0800

    fps movement/camera control

---
+++ Assets/Scripts/Player/FPSCameraControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSCameraControl : MonoBehaviour {
+	
+    public Transform follow;
+    public float sensitivity = 5f;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    private float rotationY = 0F;
+    private float rotationX = 0F;
+    private Vector3 offset;
+
+    void Awake(){
+        offset = follow.transform.position - transform.position;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update(){
+        transform.position = follow.transform.position + offset;
+        if ( !Cursor.visible ){
+            rotationX += Input.GetAxis("Mouse X") * sensitivity;
+         
+            rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+        }
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        if ( Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.Escape) ){
+            Cursor.visible = !Cursor.visible;
+            if ( !Cursor.visible ) Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/FPSMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class FPSMovement : MonoBehaviour {
+	
+    public float speed = 10f;
+
+    private Animator anim;
+    private CharacterController cc;
+    private Vector3 moveTo = Vector3.zero;
+
+    void Awake(){
+        anim = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
+    }
+    void Update(){
+        moveTo = new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical"));
+        moveTo = Camera.main.transform.TransformDirection(moveTo);
+        moveTo *= speed;
+        
+        moveTo += Physics.gravity;
+        transform.rotation = Quaternion.LookRotation(new Vector3(Camera.main.transform.forward.x,transform.position.y,Camera.main.transform.forward.z));
+
+        anim.SetFloat("speed", Input.GetAxis("Vertical"));
+        cc.Move(moveTo*Time.deltaTime);
+    }
+
+}
