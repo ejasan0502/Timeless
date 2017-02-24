27a05a718e4abb87636010984f16c4cbd9d5f450
Eric <esantiago@lockeddoorstudios.com>
2017-02-24 12:41:01 -0800

    basic movement

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -2,23 +2,35 @@
 using System.Collections;
 
 // Handles movement logic and animation
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour {
 
+    public float speed = 5f;
+
     private Animator anim;
+    private CharacterController cc;
+
     private Vector3 velocity = Vector3.zero;
 
     void Awake(){
-        // Grab animator from model
         anim = GetComponentInChildren<Animator>();
+        cc = GetComponent<CharacterController>();
     }
     void Update(){
-        Animate();
-    }
+        velocity = transform.TransformDirection(velocity);
+        velocity = velocity.normalized*speed;
 
+        velocity += Physics.gravity*Time.deltaTime;
+
+        cc.Move(velocity*Time.deltaTime);
+    }
+    
     // Animate character
-    private void Animate(){
-        anim.SetFloat(Settings.instance.anim_velocity_x, velocity.x);
-        anim.SetFloat(Settings.instance.anim_velocity_z, velocity.z);
+    public void Animate(float forward, float strafe){
+        anim.SetFloat(Settings.instance.anim_velocity_x, strafe);
+        anim.SetFloat(Settings.instance.anim_velocity_z, forward);
     }
-
+    public void Move(Vector3 velocity){
+        this.velocity = velocity;
+    }
 }
---
+++ Assets/Scripts/Player/UserInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Handles input from player other than main camera
+public class UserInput : MonoBehaviour {
+
+    private CharacterMovement charMovt;
+
+    void Awake(){
+        charMovt = GetComponent<CharacterMovement>();
+    }
+    void Update(){
+        Vector3 v = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        charMovt.Move(v);
+        charMovt.Animate(Input.GetAxis("Vertical"),Input.GetAxis("Horizontal"));
+    }
+}
