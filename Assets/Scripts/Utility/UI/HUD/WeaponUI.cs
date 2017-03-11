6b2609c66fca4b6a21d7218dce52d6407fb4078e
Eric <esantiago@lockeddoorstudios.com>
2017-03-10 17:49:10 -0800

    weapon ammo ui, removed rifle firing animation

---
+++ Assets/Scripts/Utility/UI/HUD/WeaponUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+// Handles UI for fire arms
+public class WeaponUI : UI {
+    
+    public Text ammoText;
+
+    private WeaponHandler weaponHandler;
+
+    void Start(){
+        weaponHandler = GameObject.FindWithTag("Player").GetComponent<WeaponHandler>();
+    }
+    void FixedUpdate(){
+        if ( weaponHandler && ammoText ){
+            if ( weaponHandler.currentWeapon && weaponHandler.currentWeapon.isFirearm ){
+                Firearm weapon = weaponHandler.currentWeapon as Firearm;
+                ammoText.text = weapon.clipSize + " | " + weapon.carryingAmmo;
+            } else if ( ammoText.text != "" ){
+                ammoText.text = "";
+            }
+        }
+    }
+
+}
