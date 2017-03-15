271650c465cf5332da5bb497c1a6eb71e37b1ce6
Eric <esantiago@lockeddoorstudios.com>
2017-03-15 03:52:15 -0700

    cubiquity implementation, voxel planet generation init

---
+++ Assets/Scripts/Data/Planet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Planet {
+
+    public int radius = 60;
+    public int crustDepth = 1;
+    public int mantleDepth = 10;
+    public int coreDepth = 35;
+
+    public Planet(int radius, int crustDepth, int mantleDepth, int coreDepth){
+        this.radius = radius;
+        this.crustDepth = crustDepth;
+        this.mantleDepth = mantleDepth;
+        this.coreDepth = coreDepth;
+    }
+
+}
---
+++ Assets/Scripts/Utility/VoxelGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cubiquity;
+
+// Uses Cubiquity to create voxel terrain
+public class VoxelGenerator : MonoBehaviour {
+
+    public Planet testPlanet;
+
+    void Start(){
+        CreatePlanet(testPlanet);
+    }
+
+    private void CreatePlanet(Planet planet){
+		Region volumeBounds = new Region(-planet.radius, -planet.radius, -planet.radius, planet.radius, planet.radius, planet.radius);		
+		TerrainVolumeData data = VolumeData.CreateEmptyVolumeData<TerrainVolumeData>(volumeBounds);
+			
+		TerrainVolumeGenerator.GeneratePlanet(data, planet.radius, planet.radius - planet.crustDepth, planet.radius - planet.mantleDepth, planet.radius - planet.coreDepth);
+
+        TerrainVolume terrainVol = gameObject.AddComponent<TerrainVolume>();
+        terrainVol.data = data;
+
+        gameObject.AddComponent<TerrainVolumeRenderer>();
+        gameObject.AddComponent<TerrainVolumeCollider>();
+    }
+}
